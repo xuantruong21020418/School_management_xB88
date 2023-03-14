b29e6f60df42ea26b46a22eec2ca60c6cb322d41
Trần Xuân Trường <21020418@vnu.edu.vn>
2023-03-14 08:48:28 +0700

    delete folder

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 require 'config/constants.php';
 
 //connect to the database
-$connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3307);
+$connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306);
 
 if (mysqli_errno($connection)) {
     die(mysqli_error($connection));
